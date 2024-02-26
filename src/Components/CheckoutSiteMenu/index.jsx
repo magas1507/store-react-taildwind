fdb36fc606041c36064404d4e56b5cc324948069
magas1507 <asmg15.07@gmail.com>
2024-02-26 06:48:23 -0300

    OrderCard Component

--- src/Components/CheckoutSiteMenu/index.jsx
+++ src/Components/CheckoutSiteMenu/index.jsx
@@ -2,10 +2,11 @@ import { useContext, useState } from 'react'
 import './style.css'
 import {XMarkIcon } from '@heroicons/react/24/solid'
 import { ShoppingCartContext } from '../../Context'
+import OrderCard from '../../Components/OrderCard'
 
 const checkoutSiteMenu = () =>{
   const context = useContext(ShoppingCartContext) 
-  console.log('Product to show', context.productToShow)
+  console.log('Product to show', context.cartProducts)
 
   return(
     <aside className={`${context.isCheckoutSideMenuOpen ? 'flex': 'hidden'} checkout-side-menu flex flex-col fixed right-0 border-black rounded-lg bg-white`}>
@@ -21,7 +22,18 @@ const checkoutSiteMenu = () =>{
           />
         </div>
       </div>
-     
+      <div className='px-6'>
+        {
+          context.cartProducts.map( product =>(
+            <OrderCard
+              key={product.id} 
+              title={product.title}
+              imageUrl={product.image}
+              price={product.price}
+            />
+          ))
+        }  
+      </div>
     </aside>
   )
 }
